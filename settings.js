56dc149af9efcdb97a11fb8694e9b2b81980dccd
Zubairchoohan31 <160321525+Zubairchoohan31@users.noreply.github.com>
2024-03-05 00:12:30 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,8 +16,8 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
+  OWNER_NUMBER: "923345870705",
+  SUDO_NUMBERS : "923345870705",
   OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
